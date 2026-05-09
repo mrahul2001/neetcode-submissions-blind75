96491ac6b07a0d299abb76f2c8de2af3df5114e9
Rahul Mitra <89061465+mrahul2001@users.noreply.github.com>
2026-05-09 22:59:21 +0530

    Add: shortest-path-in-binary-matrix - submission-1

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-1.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (grid[0][0] != 0 || grid[rows - 1][cols - 1] != 0)
+            return -1;
+
+        int[][] dir = {
+            {1, 0},
+            {0, 1},
+            {1, 1},
+            {-1, 0},
+            {0, -1},
+            {-1, -1},
+            {-1, 1},
+            {1, -1}
+        };
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+        grid[0][0] = 1;
+
+        int len = 1;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int it = 0; it < size; it++) {
+                int[] curr = q.poll();
+                int row = curr[0];
+                int col = curr[1];
+
+                if (row == rows - 1 && col == cols - 1)
+                    return len;
+
+                for (int i = 0; i < 8; i++) {
+                    int nRow = row + dir[i][0];
+                    int nCol = col + dir[i][1];
+
+                    if ((nRow >= 0 && nRow < rows) &&
+                    (nCol >= 0 && nCol < cols) &&
+                    grid[nRow][nCol] == 0) {
+                        q.add(new int[]{nRow, nCol});
+                        grid[nRow][nCol] = 1;
+                    }
+                }
+            }
+            len++;
+        }
+        return -1;
+    }
+}
